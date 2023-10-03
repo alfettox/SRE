25933fb2da249d98244e6deb505357c3f69f78d8
alfettox <dott.defranceschi@gmail.com>
2023-10-03 14:40:37 -0400

    SQL practice

---
+++ sql-assignment/aggregates.sql
@@ -0,0 +1,222 @@
+-- Giovanni De Franceschi TP003
+
+/*
+Aggregate Queries
+
+REQUIREMENT - Use a multi-line comment to paste the first 5 or fewer results under your query
+		     THEN records returned. 
+*/
+
+USE orderbook_activity_db;
+
+-- #1: How many users do we have?
+SELECT COUNT(userId) AS "number of users", SUM(userId) as "sum"
+from User;
+
+
+SELECT COUNT(userId) AS `Number of users` FROM User;
+
+/*
+7
+ROWS = 1
+*/
+
+-- #2: List the username, userid, and number of orders each user has placed.
+SELECT u.uname,u.userid, COUNT(o.userid) AS numOrders
+FROM User AS u
+JOIN `Order` AS o
+ON o.userid = u.userid
+GROUP BY u.uname, u.userid;
+
+
+
+
+SELECT u.uname, u.userId, COUNT(o.orderid) AS number_orders FROM `User` AS u
+JOIN `Order` AS o
+ON o.userId = u.userId
+GROUP BY u.uname, u.userId;
+
+/*
+admin	1	3
+alice	5	8
+james	3	3
+kendra	4	5
+robert	6	5
+ROWS = 5;
+*/
+
+
+-- #3: List the username, symbol, and number of orders placed for each user and for each symbol.
+-- Sort results in alphabetical order by symbol.
+
+
+
+SELECT u.uname, o.symbol, COUNT(*) AS `Order_Count`
+FROM `Order` AS o
+JOIN `User` AS u ON o.userId = u.userId
+GROUP BY u.uname, o.symbol
+ORDER BY o.symbol ASC;
+
+/*
+alice	A		5
+james	A		1
+robert	AAA		1
+admin	AAPL	1
+robert	AAPL	1
+ROWS = 19
+*/
+
+-- #4: Perform the same query as the one above, but only include admin users.
+SELECT u.uname, o.symbol, COUNT(*) AS `Order_Count`, COUNT(o.symbol)
+FROM `Order` AS o
+JOIN `User` AS u ON o.userId = u.userId
+JOIN `UserRoles` AS ur
+ON u.userid = ur.userid
+JOIN `Role` AS r
+ON ur.roleId = r.roleId
+WHERE r.name = 'admin'
+GROUP BY u.uname, o.symbol
+ORDER BY o.symbol ASC;
+
+/*
+alice	A		5	5
+admin	AAPL	1	1
+alice	GOOG	1	1
+admin	GS		1	1
+alice	SPY		1	1
+ROWS =7
+*/
+
+
+-- #5: List the username and the average absolute net order amount for each user with an order.
+-- Round the result to the nearest hundredth and use an alias (averageTradePrice).
+-- Sort the results by averageTradePrice with the largest value at the top.
+
+SELECT u.uname AS username,
+	   ROUND(SUM(ABS(o.price * o.shares))/COUNT(o.orderId),2) AS average_net_order
+FROM User AS u
+JOIN `Order` AS o
+ON u.userId = o.userId
+WHERE o.orderId IS NOT NULL
+GROUP BY u.uname
+ORDER BY average_net_order DESC;
+
+
+
+SELECT u.uname AS username, ROUND(AVG(ABS(o.price * o.shares)),2) AS average_net_order
+FROM User AS u
+JOIN `Order` AS o
+ON u.userId = o.userId
+WHERE o.orderId IS NOT NULL
+GROUP BY u.uname
+ORDER BY average_net_order DESC;
+/*
+kendra	17109.53
+admin	12182.47
+robert	10417.84
+alice	6280.26
+james	2053.73
+ROWS = 5
+*/
+
+
+-- #6: How many shares for each symbol does each user have?
+-- Display the username and symbol with number of shares.
+SELECT u.uname, f.symbol, SUM(f.share)
+FROM `User` AS u
+JOIN `Fill` AS f ON u.userId = f.userId
+GROUP BY u.uname, f.symbol;
+
+
+SELECT
+u.uname,
+f.symbol,
+SUM(f.`share`)
+FROM `User` AS u
+INNER JOIN
+`Fill` AS f ON u.userId = f.userId
+GROUP BY u.uname, f.symbol;
+/*
+admin	WLY		-10
+admin	GS		-10
+admin	AAPL	15
+alice	A		-10
+alice	SPY		-75
+ROWS = 13
+*/
+
+
+-- #7: What symbols have at least 3 orders?
+SELECT  COUNT(o.symbol) AS order_count, o.symbol
+FROM `Order` AS o
+GROUP BY o.symbol
+HAVING order_count >= 3;
+
+SELECT COUNT(o.symbol) as order_count, o.symbol
+FROM `Order` AS o
+GROUP BY o.symbol
+HAVING order_count >= 3;
+
+/*
+3	AAPL
+3	WLY
+6	A
+ROWS = 3
+*/
+
+
+-- #8: List all the symbols and absolute net fills that have fills exceeding $100.
+-- Do not include the WLY symbol in the results.
+-- Sort the results by highest net with the largest value at the top.
+SELECT  o.symbol, ABS(f.share * f.price) AS net_fill
+FROM `Order` AS o
+JOIN Fill AS f
+ON o.orderId = f.orderId
+GROUP BY o.symbol, f.share, f.price
+HAVING NOT o.symbol = 'WLY' AND net_fill >100
+ORDER BY net_fill DESC;
+
+/*
+SPY		27429.75
+SPY		27429.75
+GS		3056.30
+GS		3056.30
+AAPL	2111.40
+ROWS =  12
+*/
+
+
+-- #9: List the top five users with the greatest amount of outstanding orders.             (????????)
+-- Display the absolute amount filled, absolute amount ordered, and net outstanding.
+-- Sort the results by the net outstanding amount with the largest value at the top.
+
+SELECT 
+ uname,
+  SUM( ABS(f.`share` * f.price) ) AS abs_amt_filled,
+   SUM( ABS(o.shares*o.price )) AS abs_amt_orders,
+   SUM( o.shares + f.`share` ) AS net_outstanding
+   FROM
+    (SELECT
+     u.userid, u.uname
+     FROM
+     `User` AS u
+	JOIN `Order` AS o ON u.userid = o.userid
+    LEFT JOIN Fill AS f ON o.orderid = f.orderid
+	WHERE
+    o.status IN ('pending' , 'partial_fill')
+    GROUP BY o.userid
+    ORDER BY o.userid
+    LIMIT 5) AS top5
+    JOIN `Order` as o ON top5.userid = o.userid
+    LEFT JOIN Fill as f ON o.orderid = f.orderid
+    GROUP BY top5.userid
+    ORDER BY net_outstanding DESC;
+    
+/*
+admin	5555.00		36547.40	180
+alice	29717.95	50242.10	25
+robert	3906.30		55608.20	25
+james	2288.20		6161.20		0
+kendra	31893.65	85547.65	0
+ROWS = 5
+*/
---
+++ sql-assignment/orderbook-selects.sql
@@ -0,0 +1,167 @@
+-- Giovanni De Franceschi TP003
+
+/*
+Basic Selects
+REQUIREMENT - Use a multi-line comment to paste the first 5 or fewer results under your query
+		     Also include the total records returned.
+*/
+
+USE orderbook_activity_db;
+
+-- #1: List all users, including username and dateJoined.
+SELECT uname AS `User`, dateJoined AS `Date joined`
+FROM User;
+
+SELECT uname AS `User`, dateJoined AS `Date joined`FROM User;
+
+
+
+/*
+'admin', '2023-02-14Order 13:13:28'
+'wiley', '2023-04-01 13:13:28'
+'james', '2023-03-15 19:15:48'
+'kendra', '2023-03-15 19:16:06'
+'alice', '2023-03-15 19:16:21'
+ROWS=7
+*/
+
+-- #2: List the username and datejoined from users with the newest users at the top.
+SELECT uname, datejoined
+FROM User
+ORDER BY datejoined DESC;
+
+/*
+wiley	2023-04-01 13:13:28
+sam	    2023-03-15 19:16:59
+robert	2023-03-15 19:16:43
+alice	2023-03-15 19:16:21
+kendra	2023-03-15 19:16:06
+ROWS=7
+*/
+
+-- #3: List all usernames and dateJoined for users who joined in March 2023.
+
+SELECT uname, dateJoined
+FROM User
+WHERE dateJoined BETWEEN '2023-03-01' AND '2023-03-31';
+
+/*
+james	2023-03-15 19:15:48
+kendra	2023-03-15 19:16:06
+alice	2023-03-15 19:16:21
+robert	2023-03-15 19:16:43
+sam	    2023-03-15 19:16:59
+ROWS=5
+*/
+
+-- #4: List the different role names a user can have.
+SELECT name
+FROM Role;
+
+/*
+admin
+it
+user
+ROWS=3
+*/
+
+-- #5: List all the orders.
+SELECT * FROM `Order`;
+/*
+1	1	WLY		1	2023-03-15 19:20:35	100		38.73	partial_fill
+2	6	WLY		2	2023-03-15 19:20:50	-10		38.73	filled
+3	6	NFLX	2	2023-03-15 19:21:12	-100	243.15	pending
+4	5	A		1	2023-03-15 19:21:31	10		129.89	filled
+5	3	A		2	2023-03-15 19:21:39	-10		129.89	filled
+ROWS=24
+*/
+
+-- #6: List all orders in March where the absolute net order amount is greater than 1000.
+
+SELECT orderId, userId, symbol, orderTime, shares, price, ABS(shares * price) AS `Absolute net order`, status
+FROM `Order`
+WHERE MONTH(orderTime) = 3 AND (ABS(shares * price)) > 1000;
+
+SELECT symbol,
+	ABS(shares) AS total_shares,
+	ABS(shares * price) AS total_amount
+FROM `Order`
+WHERE MONTH(orderTime) = 3
+HAVING total_amount > 1000;
+
+/*
+WLY		100	3873.00
+NFLX	100	24315.00
+A		10	1298.90
+A		10	1298.90
+GS		100	30563.00
+ROWS = 16
+*/
+
+-- #7: List all the unique status types from orders.
+
+SELECT DISTINCT status
+FROM `Order`;
+
+/*
+partial_fill
+filled
+pending
+canceled_partial_fill
+canceled
+ROWS=5
+*/
+
+-- #8: List all pending and partial fill orders with oldest orders first.
+
+SELECT *
+FROM `Order`
+WHERE status = 'pending' OR status = 'partial_fill'
+ORDER BY orderTime;
+
+/*
+1	1	WLY	    1	2023-03-15 19:20:35	100	    38.73	partial_fill
+3	6	NFLX	2	2023-03-15 19:21:12	-100	243.15	pending
+11	5	SPY	    1	2023-03-15 19:24:21	100	    365.73	partial_fill
+12	4	QQQ	    2	2023-03-15 19:24:32	-100	268.27	pending
+13	4	QQQ	    2	2023-03-15 19:24:32	-100	268.27	pending
+ROWS=10
+*/
+
+
+-- #9: List the 10 most expensive financial products where the productType is stock.
+-- Sort the results with the most expensive product at the top
+
+SELECT *
+FROM Product
+WHERE productType = 'stock'
+ORDER BY price DESC
+LIMIT 10;
+
+/*
+207940.KS	830000.00	stock	Samsung Biologics Co.,Ltd.	2022-10-17 15:24:51
+003240.KS	715000.00	stock	Taekwang Industrial Co., Ltd.	2022-10-17 15:24:51
+000670.KS	630000.00	stock	Young Poong Corporation	2022-10-17 15:24:51
+010130.KS	616000.00	stock	Korea Zinc Company, Ltd.	2022-10-17 15:24:51
+006400.KS	605000.00	stock	Samsung SDI Co., Ltd.	2022-10-17 15:24:51
+ROWS=10
+*/
+
+
+-- #10: Display orderid, fillid, userid, symbol, and absolute net fill amount (ABSOLUTE NET FILL AMOUNT = only filled ABS(SUM shares * price))
+-- from fills where the absolute net fill is greater than $1000.
+-- Sort the results with the largest absolute net fill at the top.
+
+SELECT orderid, fillid, userid, symbol, ABS(share*price) AS absolute_net_fill_amount
+FROM Fill
+HAVING absolute_net_fill_amount >1000
+ORDER BY absolute_net_fill_amount DESC;
+
+/*
+11	11	5	SPY	27429.75
+14	12	4	SPY	27429.75
+6	5	1	GS	3056.30
+7	6	4	GS	3056.30
+8	9	6	AAPL	2111.40
+ROWS=10
+*/
